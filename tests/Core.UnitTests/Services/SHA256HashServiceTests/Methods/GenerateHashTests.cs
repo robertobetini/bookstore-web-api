4d153728be5dc59a6a8efff55d8aa4ff9cab840b
Roberto <betini.roberto@gmail.com>
2023-02-28 22:00:18 -0300

    test(SHA256HashService): add unit tests.

---
+++ tests/Core.UnitTests/Services/SHA256HashServiceTests/Methods/GenerateHashTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Core.UnitTests.Services.SHA256HashServiceTests.Methods;
+
+public class GenerateHashTests : SHA256HashServiceTests
+{
+    private const string ALGORITHM_NAME = SecurityAlgorithms.Sha256;
+
+    [Theory]
+    [InlineData("Arbitrary data.")]
+    [InlineData("Some special characters: àóÍîüãÒ^`´´Çç")]
+    public void When_Invoked_Then_ReturnsBase64EncodedHashString(string data)
+    {
+        #region Arrange
+        var hashAlgorithm = HashAlgorithm.Create(ALGORITHM_NAME);
+        var keyBytes = Encoding.UTF8.GetBytes(data);
+        var hashBytes = hashAlgorithm.ComputeHash(keyBytes);
+        var expected = Convert.ToBase64String(hashBytes);
+        #endregion
+
+        // Act
+        var result = _sut.GenerateHash(data);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+}
---
+++ tests/Core.UnitTests/Services/SHA256HashServiceTests/SHA256HashServiceTests.cs
@@ -0,0 +1,13 @@
+﻿using Core.Services;
+
+namespace Core.UnitTests.Services.SHA256HashServiceTests;
+
+public abstract class SHA256HashServiceTests
+{
+	protected readonly SHA256HashService _sut;
+
+    public SHA256HashServiceTests()
+	{
+		_sut = new SHA256HashService();
+	}
+}
