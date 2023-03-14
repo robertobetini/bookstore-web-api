f10e0af654f43c519dd48e464f16c33f562dd612
Roberto <betini.roberto@gmail.com>
2023-03-13 21:31:24 -0300

    chore: organize Program.cs.

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -3,38 +3,36 @@ using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services
-    .AddControllers()
-    .AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
-
-builder.Services
-    .AddEndpointsApiExplorer()
-    .AddSwaggerGen();
-
 ConfigureServices(builder.Services);
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app
+        .UseSwagger()
+        .UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthentication();
-app.UseAuthorization();
+app
+    .UseHttpsRedirection()
+    .UseAuthentication()
+    .UseAuthorization()
+    .UseErrorHandlingMiddleware();
 
 app.MapControllers();
-
-app.UseErrorHandlingMiddleware();
 
 app.Run();
 
 static void ConfigureServices(IServiceCollection services)
 {
     services
+        .AddControllers()
+        .AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+
+    services
+        .AddEndpointsApiExplorer()
+        .AddSwaggerGen()
         .AddCoreServices()
         .AddRepositories()
         .AddJwtTokenAuthentication();
